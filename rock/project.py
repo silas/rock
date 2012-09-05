2905b09c924684b6f0eccee788d6627e6a4ef488
Silas Sewell <silas@sewell.org>
2012-09-05 16:46:47 -0400

    Remove path and add options in rock create

--- rock/project.py
+++ rock/project.py
@@ -1,5 +1,6 @@
 import os
 import pipes
+import re
 import string
 import sys
 import yaml
@@ -9,39 +10,45 @@ from rock.process import ProcessManager
 from rock.utils import Shell
 
 
+NAME_RE = re.compile('^[a-zA-Z_]+$')
+
+
 class Project(object):
 
     def __init__(self, config):
         self.config = Config(config)
 
+    def _setup(self, shell):
+        # declare builtin functions
+        shell.write('warn() { echo "$@" >&2; }')
+        shell.write('die() { warn "$@"; exit 1; }')
+        # print commands as they're run
+        if self.config.get('verbose'):
+            shell.write('set -o verbose')
+        # don't execute commands, just print them
+        if self.config.get('dry_run'):
+            shell.write('set -o noexec')
+        # exit with error if any one command fails
+        shell.write('set -o errexit')
+        # switch to project directory
+        shell.write('cd ' + pipes.quote(self.config['path']))
+        # setup environment variables
+        if self.config.get('env'):
+            # blank line before exports
+            shell.write('')
+            for name, value in self.config['env'].items():
+                shell.write('export %s="%s"' % (name, value))
+        # blank line before command
+        shell.write('')
+
     def execute(self, command):
-        with Shell() as s:
-            # declare builtin functions
-            s.write('warn() { echo "$@" >&2; }')
-            s.write('die() { warn "$@"; exit 1; }')
-            # print commands as they're run
-            if self.config.get('verbose'):
-                s.write('set -o verbose')
-            # don't execute commands, just print them
-            if self.config.get('dry_run'):
-                s.write('set -o noexec')
-            # exit with error if any one command fails
-            s.write('set -o errexit')
-            # switch to project directory
-            s.write('cd ' + pipes.quote(self.config['path']))
-            # setup environment variables
-            if self.config.get('env'):
-                # blank line before exports
-                s.write('')
-                for name, value in self.config['env'].items():
-                    s.write('export %s="%s"' % (name, value))
-            # blank line before command
-            s.write('')
+        with Shell() as shell:
+            self._setup(shell)
             # run command and wait for results
             if isinstance(command, (list, tuple)):
-                s.write(' '.join(command))
+                shell.write(' '.join(command))
             else:
-                s.write(command)
+                shell.write(command)
 
     def execute_type(self, name, *args):
         section = '%s_%s' % (name, args[0]) if len(args) > 0 else name
@@ -67,31 +74,62 @@ class Project(object):
     def clean(self, *args):
         self.execute_type('clean', *args)
 
-    def create(self, name=None, path=None, *args):
+    def _template(self, path, args):
+        with Shell() as shell:
+            self._setup(shell)
+            # handle options
+            length = len(args)
+            skip_next = False
+            template_args = []
+            for i in xrange(length):
+                if skip_next:
+                    skip_next = False
+                    continue
+                arg = args[i]
+                if arg.startswith('--'):
+                    name, value = arg[2:], '1'
+                    if '=' in name:
+                        name, value = name.split('=', 1)
+                    elif (i + 1 < length and not args[i + 1].startswith('--')):
+                        value = args[i + 1]
+                        skip_next = True
+                    name = name.upper().replace('-', '_')
+                    if NAME_RE.match(name):
+                        # write options to shell
+                        shell.write('export ROCK_%s="%s"' %
+                                    (name, pipes.quote(value)))
+                    else:
+                        raise ConfigError('invalid argument: ' + arg)
+                else:
+                    template_args.append(arg)
+            shell.write('. ' + path)
+
+    def create(self, name=None, *args):
         if not self.config._setup:
             self.config._setup = True
-        path = self.config.data['path'] = path or self.config['path']
-        # templat paths
-        paths = self.config.paths('template')
-        # run template
+        # project path
+        path = self.config['path']
+        # if name is given try to run it, otherwise list available templates
         if name:
-            # validate path
+            # create project path if it doesn't exist
             if not os.path.exists(path):
                 os.makedirs(path)
+            # ensure its a directory
             if not os.path.isdir(path):
                 raise ConfigError('path must be a directory')
+            # ensure its empty
             if os.listdir(path):
                 raise ConfigError('directory is not empty')
             # search for template and run if it exists
-            for path in paths:
-                path = os.path.join(path, name)
-                if os.path.isfile(path):
-                    self.execute('. ' + path)
-        # display templates
+            for template_path in self.config.paths('template'):
+                template_path = os.path.join(template_path, name)
+                if os.path.isfile(template_path):
+                    # run template
+                    self._template(template_path, args)
         else:
             templates = set()
             # get paths
-            for path in paths:
+            for path in self.config.paths('template'):
                 if not os.path.isdir(path):
                     continue
                 for template in os.listdir(path):
