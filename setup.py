a808b8ef6daa87dabb8b643ccd1e84f2a2460564
Silas Sewell <silas@sewell.org>
2012-09-12 14:06:07 +0000

    Another attempt at including files

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     url='https://github.com/rockplatform/rock',
     packages=['rock'],
     package_data={'rock': [
-        'data/*/*.yml',
+        'data/*/*',
     ]},
     scripts=['scripts/rock'],
     install_requires=[
