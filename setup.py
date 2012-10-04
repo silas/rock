a530828bad9b0e4be5432981f4e049ffb2f01c4b
Silas Sewell <silas@sewell.org>
2012-10-03 22:14:05 -0400

    Only install importlib/argparse for < 2.7

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import sys
+
 from distutils.core import setup
 from rock import __version__ as version
 
@@ -7,6 +9,12 @@ applications on the Rock Platform.
 
 See http://www.rockplatform.org for more information.
 """
+
+
+install_requires = ['PyYAML']
+
+if sys.version_info.major <= 2 and sys.version_info.minor < 7:
+    install_requires += ['argparse', 'importlib']
 
 setup(
     name='rock',
@@ -18,14 +26,9 @@ setup(
     license='MIT',
     url='https://github.com/rockplatform/rock',
     packages=['rock'],
-    package_data={'rock': [
-        'data/*/*',
-    ]},
+    package_data={'rock': ['data/*/*']},
     scripts=['scripts/rock'],
-    install_requires=[
-        'PyYAML',
-        'argparse',
-    ],
+    install_requires=install_requires,
     classifiers=[
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
