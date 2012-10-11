340259e47aefd549d9518fc4741d9b4f036f261c
Silas Sewell <silas@sewell.org>
2012-10-11 09:39:27 -0400

    Add rock-runtime-python27

---
+++ rock-runtime-python27.rb
@@ -0,0 +1,42 @@
+require 'formula'
+
+class RockRuntimePython27 < Formula
+  homepage 'http://www.python.org/'
+  url 'http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tar.bz2'
+  sha1 '842c4e2aff3f016feea3c6e992c7fa96e49c9aa0'
+
+  env :std
+  keg_only 'rock'
+
+  def install_virtualenv
+    virtualenv_version = '1.8.2'
+
+    system 'curl', '-O', "http://pypi.python.org/packages/source/v/virtualenv/virtualenv-#{virtualenv_version}.tar.gz"
+    system 'tar', '-xzf', "virtualenv-#{virtualenv_version}.tar.gz"
+
+    Dir.chdir "virtualenv-#{virtualenv_version}"
+
+    system 'python', 'setup.py', 'install'
+
+    Dir.chdir '..'
+  end
+
+  def install
+    ENV.append 'EXTRA_CFLAGS', '-fwrapv'
+
+    system './configure', "--prefix=#{prefix}"
+    ENV.j1
+    system 'make', 'install'
+
+    ENV['LDFLAGS'] = "-Wl,-rpath #{prefix}/lib"
+
+    system './configure',
+      "--prefix=#{prefix}",
+      '--enable-ipv6',
+      '--enable-shared'
+
+    ENV['PATH'] = "#{bin}:#{ENV['PATH']}"
+
+    install_virtualenv
+  end
+end
