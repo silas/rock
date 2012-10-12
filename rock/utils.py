062b8ae406e85dffc1337237237b0a8acdfb9745
Silas Sewell <silas@sewell.org>
2012-10-11 22:57:14 -0400

    Tweak utils.Shell style

--- rock/utils.py
+++ rock/utils.py
@@ -4,11 +4,16 @@ import os
 
 class Shell(object):
 
-    def __enter__(self):
+    def __init__(self):
         self.stdin = StringIO.StringIO()
+
+    def __enter__(self):
         return self
 
     def __exit__(self, type, value, traceback):
+        self.run()
+
+    def run(self):
         os.execl('/bin/bash', '-l', '-c', self.stdin.getvalue())
 
     def write(self, text):
