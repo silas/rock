0e447ff2291738af5cd53a29d9e1f5f41def937a
Silas Sewell <silas@sewell.org>
2013-03-21 00:02:50 -0400

    Add host to fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,15 @@
 from fabric.api import *
 
+env.roledefs['dl'] = ['dl.rockstack.org']
+
 def rsync(args):
     return 'rsync --exclude=repodata -av %s' % args
 
 def sync_local_down():
-    local(rsync('dl.rockstack.org:build/epel/6/ rpm/build/epel/6/'))
+    local(rsync('%s:build/epel/6/ rpm/build/epel/6/' % env.roledefs['dl'][0]))
 
 def sync_local():
-    local(rsync('rpm/build/epel/6/ dl.rockstack.org:build/epel/6/'))
+    local(rsync('rpm/build/epel/6/ %s:build/epel/6/' % env.roledefs['dl']))
 
 def sync_repo(name):
     root = '/srv/dl.rockstack.org/rpm/%s/el/6' % name
@@ -18,8 +20,10 @@ def sync_repo(name):
         with cd('%s/%s' % (root, arch)):
             sudo('createrepo --update .')
 
+@roles('dl')
 def sync_testing():
     sync_repo('testing')
 
+@roles('dl')
 def sync_stable():
     sync_repo('stable')
