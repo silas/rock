48d52146e42ecad58cc92b86304965a780eefa6a
Silas Sewell <silas@sewell.org>
2013-04-08 08:22:08 -0400

    Remove unused future imports

--- rock/__init__.py
+++ rock/__init__.py
@@ -3,7 +3,7 @@
 #
 # This file is subject to the MIT License (see the LICENSE file).
 
-from __future__ import absolute_import, print_function, unicode_literals
+from __future__ import unicode_literals
 
 __copyright__ = '2013, Silas Sewell'
 __version__ = '0.15.0'
