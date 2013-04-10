59ee1bac4bf839e4d6c3f87f33717b97b93a5d56
Philip Reichenberger <preichenberger@philcolabs.com>
2013-04-10 18:28:47 -0400

    added initial rock formula

---
+++ rock.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Rock < Formula
+  homepage 'http://www.rockstack.org/'
+  url 'https://pypi.python.org/packages/source/r/rock/rock-0.15.0.tar.gz'
+  sha1 '4b0c436329cbb13d4dd55423f12b094da6a3cc3c'
+
+  env :std
+
+  def install
+    system 'python', "setup.py"
+  end
+end
