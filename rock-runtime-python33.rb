54b02722657d3803126e82064ab130c0a2b52425
Silas Sewell <silas@sewell.org>
2013-04-10 17:31:11 -0400

    Python 3.3.1

--- rock-runtime-python33.rb
+++ rock-runtime-python33.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class RockRuntimePython33 < Formula
   homepage 'http://www.python.org/'
-  url 'http://www.python.org/ftp/python/3.3.0/Python-3.3.0.tar.bz2'
-  sha1 '3e1464bc2c1dfa74287bc58da81168f50b0ae5c7'
+  url 'http://www.python.org/ftp/python/3.3.1/Python-3.3.1.tar.bz2'
+  sha1 'bec78674847a4dacc4717c93b32b6b07adb90afe'
 
   env :std
   keg_only 'rock'
@@ -14,7 +14,7 @@ class RockRuntimePython33 < Formula
   end
 
   def install_distribute
-    distribute_version = '0.6.28'
+    distribute_version = '0.6.36'
 
     system 'curl', '-LO', "https://bitbucket.org/tarek/distribute/raw/#{distribute_version}/distribute_setup.py"
     system "sed 's|#!python|#!/usr/bin/env python|g' distribute_setup.py > #{bin}/distribute-setup"
