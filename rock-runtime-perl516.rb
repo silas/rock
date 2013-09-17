e90f30332ff1260a84f1fd6547644b285941799b
Silas Sewell <silas@sewell.org>
2013-09-17 12:03:00 -0700

    Fix perl516 carton install

--- rock-runtime-perl516.rb
+++ rock-runtime-perl516.rb
@@ -32,26 +32,31 @@ class RockRuntimePerl516 < Formula
     carton_version = '0.9.10'
 
     urls = [
-      'http://www.cpan.org/authors/id/T/TO/TOKUHIROM/Test-Requires-0.06.tar.gz',
-      'http://www.cpan.org/authors/id/D/DA/DAGOLDEN/Capture-Tiny-0.21.tar.gz',
-      'http://www.cpan.org/authors/id/M/MI/MIYAGAWA/App-cpanminus-1.6008.tar.gz',
-      'http://www.cpan.org/authors/id/D/DA/DAGOLDEN/CPAN-Meta-YAML-0.008.tar.gz',
-      'http://www.cpan.org/authors/id/M/MI/MIYAGAWA/Module-CPANfile-0.9010.tar.gz',
-      'http://www.cpan.org/authors/id/D/DA/DAGOLDEN/Parse-CPAN-Meta-1.4404.tar.gz',
-      'http://www.cpan.org/authors/id/D/DA/DAGOLDEN/CPAN-Meta-Requirements-2.122.tar.gz',
-      'http://www.cpan.org/authors/id/D/DA/DAGOLDEN/CPAN-Meta-2.120921.tar.gz',
-      'http://www.cpan.org/authors/id/T/TM/TMTM/Class-Data-Inheritable-0.08.tar.gz',
-      'http://www.cpan.org/authors/id/D/DR/DROLSKY/Devel-StackTrace-1.30.tar.gz',
-      'http://www.cpan.org/authors/id/D/DR/DROLSKY/Exception-Class-1.37.tar.gz',
-      'http://www.cpan.org/authors/id/M/ML/MLEHMANN/common-sense-3.6.tar.gz',
-      'http://www.cpan.org/authors/id/M/ML/MLEHMANN/JSON-XS-2.33.tar.gz',
-      'http://www.cpan.org/authors/id/M/MA/MAKAMAKA/JSON-2.53.tar.gz',
-      'http://www.cpan.org/authors/id/D/DO/DOY/Try-Tiny-0.12.tar.gz',
-      'http://www.cpan.org/authors/id/A/AP/APEIRON/local-lib-1.008009.tar.gz',
-      "http://www.cpan.org/authors/id/M/MI/MIYAGAWA/carton-v#{carton_version}.tar.gz",
+      'http://backpan.cpan.org/authors/id/T/TO/TOKUHIROM/Test-Requires-0.06.tar.gz',
+      'http://backpan.cpan.org/authors/id/D/DA/DAGOLDEN/Capture-Tiny-0.21.tar.gz',
+      'http://backpan.cpan.org/authors/id/M/MI/MIYAGAWA/App-cpanminus-1.6008.tar.gz',
+      'http://backpan.cpan.org/authors/id/D/DA/DAGOLDEN/CPAN-Meta-YAML-0.008.tar.gz',
+      'http://backpan.cpan.org/authors/id/M/MI/MIYAGAWA/Module-CPANfile-0.9010.tar.gz',
+      'http://backpan.cpan.org/authors/id/D/DA/DAGOLDEN/Parse-CPAN-Meta-1.4404.tar.gz',
+      'http://backpan.cpan.org/authors/id/D/DA/DAGOLDEN/CPAN-Meta-Requirements-2.122.tar.gz',
+      'http://backpan.cpan.org/authors/id/D/DA/DAGOLDEN/CPAN-Meta-2.120921.tar.gz',
+      'http://backpan.cpan.org/authors/id/T/TM/TMTM/Class-Data-Inheritable-0.08.tar.gz',
+      'http://backpan.cpan.org/authors/id/D/DR/DROLSKY/Devel-StackTrace-1.30.tar.gz',
+      'http://backpan.cpan.org/authors/id/D/DR/DROLSKY/Exception-Class-1.37.tar.gz',
+      'http://backpan.cpan.org/authors/id/M/ML/MLEHMANN/common-sense-3.6.tar.gz',
+      'http://backpan.cpan.org/authors/id/M/ML/MLEHMANN/JSON-XS-2.33.tar.gz',
+      'http://backpan.cpan.org/authors/id/M/MA/MAKAMAKA/JSON-2.53.tar.gz',
+      'http://backpan.cpan.org/authors/id/D/DO/DOY/Try-Tiny-0.12.tar.gz',
+      'http://backpan.cpan.org/authors/id/A/AP/APEIRON/local-lib-1.008009.tar.gz',
+      "http://backpan.perl.org/authors/id/M/MI/MIYAGAWA/carton-v#{carton_version}.tar.gz",
     ]
 
+    home = ENV['HOME']
+    ENV['HOME'] = Dir.getwd
+
     system 'cpanm', '-l', 'local', *urls
+
+    ENV['HOME'] = home
 
     system 'mv', 'local', "#{prefix}/lib/carton"
 
