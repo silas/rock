fd217f4a234d05027e1ed5ee6557c6294765d9b3
Silas Sewell <silas@sewell.org>
2013-09-20 21:57:54 -0700

    build.bash style tweaks

--- deb/build.bash
+++ deb/build.bash
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+
 set -e
 
-# Constants 
+# Constants
 ARCHS=( i386 amd64 )
 ALL_ARCH=i386
 DISTS=( raring precise quantal squeeze wheezy )
@@ -15,31 +16,25 @@ export DEBFULLNAME=RockStack
 export PBUILDFOLDER=$PBUILDER_DIR
 
 # Paths
-cd `dirname $0`
-package_root=`pwd`
-packages=`cat $package_root/build.list`
+cd $( dirname $0 )
+package_root=$( pwd )
+packages=$( cat $package_root/build.list )
 
-# Args
-package=$1
-dist=$2
-arch=$3
-
-function usage() {
-  echo "Usage: `basename $0` [-p package -d distribution -a architecture]"
+usage() {
+  echo "Usage: $( basename $0 ) [-p package -d distribution -a architecture]"
   exit 0
 }
 
-function echo_normal() {
+display() {
   echo "-----> $1"
 }
 
-function setup_env() {
+setup_env() {
   dist=$1
   arch=$2
 
   pbuilder_name=$dist
-  if [ "$arch" == 'i386' ]
-  then
+  if [ "$arch" == 'i386' ]; then
     pbuilder_name=${pbuilder_name}-${arch}
   fi
 
@@ -48,29 +43,28 @@ function setup_env() {
   pbuilder_name=${pbuilder_name}-base.tgz
 
   mkdir -p $pbuilder_package_mount
-cat << HERE > $pbuilder_package_mount/D05deps
+cat << EOF > $pbuilder_package_mount/D05deps
 #!/usr/bin/env bash
 
 echo "deb file://$pbuilder_package_mount ./" >> /etc/apt/sources.list
 apt-get update
-HERE
+EOF
 
   chmod +x $pbuilder_package_mount/D05deps
   pbuilder_create_args=''
   pbuilder_create_args="$pbuilder_args --debootstrapopts --variant=buildd"
 
-  pbuilder_size=`du $PBUILDER_DIR/$pbuilder_name | awk '{print $1}'`
-  if [ ! -e "$PBUILDER_DIR/$pbuilder_name" ] || [ "$pbuilder_size" == 0 ]
-  then
-    echo_normal "Environment $dist-$arch does not exist, creating..."
-    echo_normal "Running: pbuilder-dist $dist $arch create $pbuilder_args"
+  pbuilder_size=$( du $PBUILDER_DIR/$pbuilder_name | awk '{print $1}' )
+  if [ ! -e "$PBUILDER_DIR/$pbuilder_name" ] || [ "$pbuilder_size" == 0 ]; then
+    display "Environment $dist-$arch does not exist, creating..."
+    display "Running: pbuilder-dist $dist $arch create $pbuilder_args"
     pbuilder-dist $dist $arch create $pbuilder_create_args
   fi
 
   apt_packages=$pbuilder_package_mount/Packages
 }
 
-function build() {
+build() {
   # args
   package=$1
   dist=$2
@@ -78,28 +72,27 @@ function build() {
 
   # values
   cd $package_root/$package
-  package_dist=`dpkg-parsechangelog | grep Distribution | awk '{print $2}'`
-  package_version=`dpkg-parsechangelog | grep Version | awk '{print $2}'`
-  package_arch=`grep Architecture debian/control | head -1 | awk '{print $2}'`
+  package_dist=$( dpkg-parsechangelog | grep Distribution | awk '{print $2}' )
+  package_version=$( dpkg-parsechangelog | grep Version | awk '{print $2}' )
+  package_arch=$( grep Architecture debian/control | head -1 | awk '{print $2}' )
 
   case $dist in
   squeeze)
     package_suffix='~bp6.0+'
     package_debiandist=stable
-  ;;
+    ;;
   wheezy)
     package_suffix='~bp7.0+'
     package_debiandist=testing
-  ;;
+    ;;
   *)
     package_suffix="~${dist}"
     package_debiandist=$dist
-  ;;
+    ;;
   esac
 
   # Setup env
-  if [ "$package_arch" == 'all' ]
-  then
+  if [ "$package_arch" == 'all' ]; then
     setup_env $dist $ALL_ARCH
     package_arch=all
   else
@@ -108,10 +101,10 @@ function build() {
   fi
 
   deb_search=${package}_${package_version}${package_suffix}*_${package_arch}.deb
-  match_deb=`find $pbuilder_package_mount -type f -name "$deb_search"`
-  if [ "$match_deb" != '' ]
-  then
-    echo_normal "$match_deb found, skipping"
+  match_deb=$( find $pbuilder_package_mount -type f -name "$deb_search" )
+
+  if [ -n "$match_deb" ]; then
+    display "$match_deb found, skipping"
     return
   fi
 
@@ -120,16 +113,14 @@ function build() {
   rm -rf $build_dir
   mkdir -p $build_dir
   cp -rf $package_root/$package/ $build_dir/package/
-  
+
   cd $build_dir/package
-  echo_normal "Building package: $package"
-  if [ "$package_dist" != "$dist" ]
-  then
+  display "Building package: $package"
+  if [ "$package_dist" != "$dist" ]; then
     dch -l $package_suffix -D $package_debiandist "Backport to $dist"
   fi
 
-  if [ -e 'debian/watch' ]
-  then
+  if [ -e 'debian/watch' ]; then
     debian/rules get-orig-source
   fi
 
@@ -140,7 +131,7 @@ function build() {
   cd $pbuilder_package_mount
   apt-ftparchive packages .  > $apt_packages
 
-  # Build  
+  # Build
   pbuilder_args="--hookdir $pbuilder_package_mount --allow-untrusted --bindmounts $PBUILDER_MOUNT"
   pbuilder-dist $dist $arch build $pbuilder_args $build_dir/$package_$version*.dsc
 
@@ -149,84 +140,74 @@ function build() {
   mv $pbuilder_result_mount/*.deb $pbuilder_package_mount/
 }
 
-dist=all
-arch=all
-package=all
+main() {
+  dist=all
+  arch=all
+  package=all
 
-while getopts "hd:a:p:" opt
-do
-  case $opt in
-    h)
-      usage
-      exit 0
-      ;;
-    d)
-      if [ ! -z "$OPTARG" ]
-      then
-        dist=$OPTARG
-      fi
-      ;;
-    a)
-      if [ ! -z "$OPTARG" ]
-      then
-        arch=$OPTARG
-      fi
-      ;;
-    p)
-      if [ ! -z "$OPTARG" ]
-      then
-        package=$OPTARG
-      fi
-      ;;
-    \?)
-      usage
-      exit 1
-      ;;
-    :)
-      echo "$OPTARG requires an argument"
-      exit 1
-      ;;
-  esac 
-done
+  while getopts "hd:a:p:" opt; do
+    case $opt in
+      h)
+        usage
+        exit 0
+        ;;
+      d)
+        [ -n "$OPTARG" ] && dist="$OPTARG"
+        ;;
+      a)
+        [ -n "$OPTARG" ] && arch="$OPTARG"
+        ;;
+      p)
+        [ -n "$OPTARG" ] && package="$OPTARG"
+        ;;
+      \?)
+        usage
+        exit 1
+        ;;
+      :)
+        echo "$OPTARG requires an argument"
+        exit 1
+        ;;
+    esac
+  done
 
-# Ensure build deps are installed
-echo_normal "Ensuring build tools are installed..."
-apt-get install ubuntu-dev-tools debhelper dh-make reprepro debootstrap pbuilder libcrypt-ssleay-perl -y
+  # Ensure build deps are installed
+  display "Ensuring build tools are installed..."
+  apt-get install ubuntu-dev-tools debhelper dh-make reprepro debootstrap pbuilder libcrypt-ssleay-perl -y
 
-# Ensure local apt is available
-mkdir -p $PBUILDER_MOUNT
+  # Ensure local apt is available
+  mkdir -p $PBUILDER_MOUNT
 
-# Ensure local var dir is available
-mkdir -p $VAR_DIR
+  # Ensure local var dir is available
+  mkdir -p $VAR_DIR
 
-# Process
-if [ "$package" == 'all' ]
-then
-  selected_packages=${packages[@]}
-else
-  selected_packages=$package
-fi
-if [ "$dist" == 'all' ]
-then
-  selected_dists=${DISTS[@]}
-else
-  selected_dists=$dist
-fi
-if [ "$arch" == 'all' ]
-then
-  selected_archs=${ARCHS[@]}
-else
-  selected_archs=$arch
-fi
+  # Process
+  if [ "$package" == 'all' ]; then
+    selected_packages=${packages[@]}
+  else
+    selected_packages=$package
+  fi
 
-for d in ${selected_dists[@]}
-do
-  for a in ${selected_archs[@]}
-  do
-    for p in ${selected_packages[@]}
-    do
-      echo_normal "Processing: $p-$d-$a"
-      build $p $d $a
+  if [ "$dist" == 'all' ]; then
+    selected_dists=${DISTS[@]}
+  else
+    selected_dists=$dist
+  fi
+
+  if [ "$arch" == 'all' ]; then
+    selected_archs=${ARCHS[@]}
+  else
+    selected_archs=$arch
+  fi
+
+  for d in ${selected_dists[@]}; do
+    for a in ${selected_archs[@]}; do
+      for p in ${selected_packages[@]}; do
+        display "Processing: $p-$d-$a"
+        build $p $d $a
+      done
     done
   done
-done
+}
+
+main $@
