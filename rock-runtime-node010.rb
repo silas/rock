3d69b4e574f00234c44406f3fa456a8ad51b0102
Silas Sewell <silas@sewell.org>
2013-10-03 15:15:53 -0700

    Fix clang related segfault issue
    See https://github.com/joyent/node/commit/5d4ac27

--- rock-runtime-node010.rb
+++ rock-runtime-node010.rb
@@ -7,6 +7,10 @@ class RockRuntimeNode010 < Formula
 
   keg_only 'rock'
 
+  def patches
+    DATA
+  end
+
   def install
     rock = Pathname.new('/opt/rock')
 
@@ -28,3 +32,17 @@ class RockRuntimeNode010 < Formula
     EOS
   end
 end
+
+__END__
+--- ./deps/v8/src/spaces.h.orig	2013-10-03 15:11:58.000000000 -0700
++++ ./deps/v8/src/spaces.h	2013-10-03 15:12:14.000000000 -0700
+@@ -320,7 +320,8 @@
+   Space* owner() const {
+     if ((reinterpret_cast<intptr_t>(owner_) & kFailureTagMask) ==
+         kFailureTag) {
+-      return reinterpret_cast<Space*>(owner_ - kFailureTag);
++      return reinterpret_cast<Space*>(reinterpret_cast<intptr_t>(owner_) -
++                                      kFailureTag);
+     } else {
+       return NULL;
+     }
--- rock-runtime-node08.rb
+++ rock-runtime-node08.rb
@@ -7,6 +7,10 @@ class RockRuntimeNode08 < Formula
 
   keg_only 'rock'
 
+  def patches
+    DATA
+  end
+
   def install
     rock = Pathname.new('/opt/rock')
 
@@ -28,3 +32,17 @@ class RockRuntimeNode08 < Formula
     EOS
   end
 end
+
+__END__
+--- ./deps/v8/src/spaces.h.orig	2013-10-03 15:05:55.000000000 -0700
++++ ./deps/v8/src/spaces.h	2013-10-03 15:06:35.000000000 -0700
+@@ -321,7 +321,8 @@
+   Space* owner() const {
+     if ((reinterpret_cast<intptr_t>(owner_) & kFailureTagMask) ==
+         kFailureTag) {
+-      return reinterpret_cast<Space*>(owner_ - kFailureTag);
++      return reinterpret_cast<Space*>(reinterpret_cast<intptr_t>(owner_) -
++                                      kFailureTag);
+     } else {
+       return NULL;
+     }
