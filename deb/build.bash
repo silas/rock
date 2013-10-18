608b6b846a23256ea10afa0ba5d2c30f5add8520
Philip Reichenberger <preichenberger@philcolabs.com>
2013-10-18 17:23:35 -0400

    Added saucy

--- deb/build.bash
+++ deb/build.bash
@@ -5,7 +5,7 @@ set -e
 # Constants
 ARCHS=( i386 amd64 )
 ALL_ARCH=i386
-DISTS=( raring precise quantal squeeze wheezy )
+DISTS=( saucy raring precise quantal squeeze wheezy )
 ROOT_DIR=/root/rock-build
 PBUILDER_DIR=${ROOT_DIR}/pbuilder
 PBUILDER_MOUNT=${ROOT_DIR}/apt
