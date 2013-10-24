d8f8d8aa8a16f57cb76d6912b3cebaab0bc66085
Silas Sewell <silas@sewell.org>
2013-10-23 21:24:32 -0400

    Fix python33 on Mavericks

--- rock-runtime-python33.rb
+++ rock-runtime-python33.rb
@@ -8,12 +8,9 @@ class RockRuntimePython33 < Formula
   env :std
   keg_only 'rock'
 
-  def install_distribute
-    distribute_version = '0.6.36'
-
-    system 'curl', '-LO', "https://bitbucket.org/tarek/distribute/raw/#{distribute_version}/distribute_setup.py"
-    system "sed 's|#!python|#!/usr/bin/env python|g' distribute_setup.py > #{bin}/distribute-setup"
-    system 'chmod', '755', "#{bin}/distribute-setup"
+  resource 'distribute' do
+    url 'https://bitbucket.org/tarek/distribute/raw/0.6.36/distribute_setup.py'
+    sha1 '69561619041d9814e747596747d58c121fe2d996'
   end
 
   def install
@@ -26,20 +23,38 @@ class RockRuntimePython33 < Formula
 
     ENV.append 'EXTRA_CFLAGS', '-fwrapv'
 
-    system './configure', "--prefix=#{prefix}"
-    ENV.j1
+    if ENV.compiler == :clang
+      ENV.append_to_cflags '-Wno-unused-value'
+      ENV.append_to_cflags '-Wno-empty-body'
+      ENV.append_to_cflags '-Qunused-arguments'
+    else
+      ENV.append 'LDFLAGS', "-Wl,-rpath #{prefix}/lib"
+    end
+
+    unless MacOS::CLT.installed?
+      ENV.append_to_cflags "-I#{MacOS.sdk_path}/usr/include"
+    end
+
+    args = %W[
+      --prefix=#{prefix}
+      --enable-ipv6
+      --enable-shared
+    ]
+    args << '--without-gcc' if ENV.compiler == :clang
+
+    system './configure', *args
+    system 'make'
+    ENV.deparallelize
     system 'make', 'install'
 
-    ENV['LDFLAGS'] = "-Wl,-rpath #{prefix}/lib"
-
-    system './configure',
-      "--prefix=#{prefix}",
-      '--enable-ipv6',
-      '--enable-shared'
+    File.symlink("#{bin}/python3", "#{bin}/python")
 
     ENV['PATH'] = "#{bin}:#{ENV['PATH']}"
 
-    install_distribute
+    resource('distribute').stage {
+      system "sed 's|#!python|#!/usr/bin/env python|g' distribute_setup.py > #{bin}/distribute-setup"
+      system 'chmod', '755', "#{bin}/distribute-setup"
+    }
 
     runtime = rock + 'runtime/python33'
     runtime.mkpath
