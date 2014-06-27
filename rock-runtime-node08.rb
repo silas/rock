c5fd0f70b8410b9f89961f2f7a146792e70e2c70
Silas Sewell <silas@sewell.org>
2014-06-27 01:43:54 -0700

    node08: 0.8.27

--- rock-runtime-node08.rb
+++ rock-runtime-node08.rb
@@ -2,14 +2,10 @@ require 'formula'
 
 class RockRuntimeNode08 < Formula
   homepage 'http://nodejs.org/'
-  url 'http://nodejs.org/dist/v0.8.26/node-v0.8.26.tar.gz'
-  sha1 '2ec960bcc8cd38da271f83c1b2007c12da5153b3'
+  url 'http://nodejs.org/dist/v0.8.27/node-v0.8.27.tar.gz'
+  sha1 '13ebdc277c76732089982b29fb25941dbee5af3f'
 
   keg_only 'rock'
-
-  def patches
-    DATA
-  end
 
   def install
     system './configure', "--prefix=#{prefix}"
@@ -28,17 +24,3 @@ class RockRuntimeNode08 < Formula
     File.symlink(prefix, runtime)
   end
 end
-
-__END__
---- ./deps/v8/src/spaces.h.orig	2013-10-03 15:05:55.000000000 -0700
-+++ ./deps/v8/src/spaces.h	2013-10-03 15:06:35.000000000 -0700
-@@ -321,7 +321,8 @@
-   Space* owner() const {
-     if ((reinterpret_cast<intptr_t>(owner_) & kFailureTagMask) ==
-         kFailureTag) {
--      return reinterpret_cast<Space*>(owner_ - kFailureTag);
-+      return reinterpret_cast<Space*>(reinterpret_cast<intptr_t>(owner_) -
-+                                      kFailureTag);
-     } else {
-       return NULL;
-     }
