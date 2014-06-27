89c079fef77abe871ce510bb04d8d1e7ab7d7e33
Silas Sewell <silas@sewell.org>
2014-06-27 01:43:08 -0700

    rock: 0.19.0

--- rock-cli.rb
+++ rock-cli.rb
@@ -2,15 +2,15 @@ require 'formula'
 
 class RockCli < Formula
   homepage 'http://www.rockstack.org/'
-  url 'https://pypi.python.org/packages/source/r/rock/rock-0.18.1.tar.gz'
-  sha1 '643940afc92cc2a90323dfd09482bd82d2ee296e'
+  url 'https://pypi.python.org/packages/source/r/rock/rock-0.19.0.tar.gz'
+  sha1 '4c5e1cdd7c109a4913386c5319dbe6fdf78c766c'
 
   depends_on :python
   depends_on 'libyaml'
 
   resource 'virtualenv' do
-    url 'https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.11.tar.gz'
-    sha1 '1f61baf4963a4bbea116049f6970962d6add641f'
+    url 'https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.11.6.tar.gz'
+    sha1 'd3f8e94bf825cc999924e276c8f1c63b8eeb0715'
   end
 
   def install
