50afa64a262644f6f05426407d40813dc79b667d
Silas Sewell <silas@sewell.org>
2014-09-18 23:04:07 -0400

    node010: 0.10.32

--- rock-runtime-node010.rb
+++ rock-runtime-node010.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class RockRuntimeNode010 < Formula
   homepage 'http://nodejs.org/'
-  url 'http://nodejs.org/dist/v0.10.31/node-v0.10.31.tar.gz'
-  sha1 '80f2160b0525763b557742aa73d8dacf1a71e53c'
+  url 'http://nodejs.org/dist/v0.10.32/node-v0.10.32.tar.gz'
+  sha1 '1d748171ba2a9568853ccec442c5f62c46fccc20'
 
   keg_only 'rock'
 
